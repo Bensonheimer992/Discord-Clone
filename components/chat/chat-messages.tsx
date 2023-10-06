443da74565a8e2ba8b146fcf15bbb75579b6f5d8
Bensonheimer992 <101340634+Bensonheimer992@users.noreply.github.com>
2023-10-06 20:30:49 +0200

    Added Private Messages and Prepared Video and Audio Calls

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -47,7 +47,10 @@ interface ChatMessagesProps {
 
    const queryKey = `chat:${chatId}`;
    const addKey = `chat:${chatId}:messages`;
-   const updateKey = `chat:${chatId}:messages:update` 
+   const updateKey = `chat:${chatId}:messages:update`
+
+   const chatRef = useRef<ElementRef<"div">>(null);
+   const bottomRef = useRef<ElementRef<"div">>(null);
 
    const {
       data,
@@ -61,6 +64,14 @@ interface ChatMessagesProps {
       paramKey,
       paramValue
    });
+   useChatSocket({ queryKey, addKey, updateKey })
+   useChatScroll({
+    chatRef,
+    bottomRef,
+    loadMore: fetchNextPage,
+    shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+    count: data?.pages?.[0]?.items?.length ?? 0,
+  })
 
    if (status === "loading") {
       return (
@@ -85,12 +96,23 @@ interface ChatMessagesProps {
     }
     
     return ( 
-        <div className="flex-1 flex flex-col py-4 overflow-y-auto">
-           <div className="flex-1"/> 
-           <ChatWelcome 
-            type={type}
-            name={name}
-           />
+        <div ref={chatRef}className="flex-1 flex flex-col py-4 overflow-y-auto">
+           {!hasNextPage && <div className="flex-1"/>} 
+            {!hasNextPage && (<ChatWelcome 
+              type={type}
+              name={name}
+           />)}
+           {hasNextPage && (
+            <div className="flex justify-center">
+              {isFetchingNextPage ? (
+                <Loader2 className="h-6 w-6 text-zinc-500 animate-spin my-4"/>
+              ) : (
+                <button className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 text-xs my-4 dark:hover:text-zinc-300 transition" onClick={() => fetchNextPage()}>
+                  Load more Messages
+                </button>
+              )}
+            </div>
+           )}
            <div className="flex flex-col-reverse mt-aut">
             {data?.pages?.map((group, i) => (
               <Fragment key={i}>
@@ -112,6 +134,7 @@ interface ChatMessagesProps {
               </Fragment>
             ))}
            </div>
+           <div ref={bottomRef}/>
         </div>
      );
 }
